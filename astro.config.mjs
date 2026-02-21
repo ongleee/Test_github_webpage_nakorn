918b35823bd9a7a01f7be282889fb947640fa287
ongleee <henry49henry.49@gmail.com>
2026-02-21 17:43:57 +0700

    change to astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+
+  site: 'https://ongleee.github.io',
+  base: 'Test_github_webpage_nakorn',
+});
